9b5212f598508e784f43f291341bb4639898d0ba
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-06 13:20:48 -0700

    container and marketing scaffolding

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+	return (
+		<div>
+			<h1>hello container</h1>
+			<MarketingApp />
+		</div>
+	);
+};
